95f53a1fa095ee18d909bc01fe3349f641450fb1
Tyler Lawson <tylerjlawson2@gmail.com>
2021-07-06 20:12:19 -0400

    fixed script run

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-var toml = require('toml');
-var concat = require('concat-stream');
-var fs = require('fs');
-const shell = require('shelljs');
+#!/usr/bin/env node
+
+var toml = require("toml");
+var concat = require("concat-stream");
+var fs = require("fs");
+const shell = require("shelljs");
 
 try {
-  if (!fs.existsSync('exampleSite/netlify.toml')) {
+  if (!fs.existsSync("netlify.toml")) {
     console.warn(
-      'Missing netlify.toml in exampleSite directory\nFor more info: https://gohugo.io/hosting-and-deployment/hosting-on-netlify/'
+      "Missing netlify.toml in exampleSite directory\nFor more info: https://gohugo.io/hosting-and-deployment/hosting-on-netlify/"
     );
   }
 } catch (e) {
-  console.log('An error occurred when trying to read netlify.toml:', e);
+  console.log("An error occurred when trying to read netlify.toml:", e);
 }
 
-fs.createReadStream('../theme.toml', 'utf8').pipe(
+fs.createReadStream("../theme.toml", "utf8").pipe(
   concat(function (data) {
     const parsed = toml.parse(data);
     const themeURL = parsed.homepage;
-    const themeURLSplit = parsed.homepage.split('/');
+    const themeURLSplit = parsed.homepage.split("/");
     const themeName = themeURLSplit[themeURLSplit.length - 1];
 
-    if (!'demosite' in parsed) console.warn('Missing demourl in theme.toml');
+    if (!"demosite" in parsed) console.warn("Missing demourl in theme.toml");
 
     try {
       if (fs.existsSync(`themes/${themeName}`)) {
-        console.log('Theme exists.');
+        console.log("Theme exists.");
       } else {
         shell.exec(`git clone ${themeURL}.git themes/${themeName}`);
       }
     } catch (e) {
-      console.log('An error occurred when trying to clone:', e);
+      console.log("An error occurred when trying to clone:", e);
     }
-    shell.exec('hugo');
+    shell.exec("hugo");
   })
 );
